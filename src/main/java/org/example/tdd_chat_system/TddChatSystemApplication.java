5cb1b8622503b97ce166f891e3912db282953abe
Chanzhou Li <leon3309@gmail.com>
2021-09-27 14:57:19 +0200

    Initial commit

---
+++ src/main/java/org/example/tdd_chat_system/TddChatSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.tdd_chat_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddChatSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddChatSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/tdd_chat_system/TddChatSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tdd_chat_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddChatSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
